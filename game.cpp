da42dd088c84524d0af028f83548c3c19c3d7280
ltg001 <1071629548@qq.com>
2019-05-07 14:45:18 +0800

    game time counter added

--- game.cpp
+++ game.cpp
@@ -1,42 +1,49 @@
+#include "game.h"
 #include "include.h"
 #include "ui_game.h"
-#include "game.h"
 
-game::game(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::game)
-{
+game::game( QWidget *parent )
+    : QDialog( parent )
+    , ui( new Ui::game ) {
     QString s = "";
-    ui->setupUi(this);
+    ui->setupUi( this );
 }
 
-game::~game()
-{
-    delete ui;
+game::~game() { delete ui; }
+
+void game::set_text( const QString _s ) { ui->question->setText( _s ); }
+
+void game::cancel_context() { ui->question->setText( "" ); }
+
+void game::start_game( const QString _s, int level ) {
+    this->set_text( _s );
+    QTimer::singleShot( 1000 * ( 6 - level ), this, SLOT( cancel_context() ) );
+
+    QTimer *update_left_time = new QTimer( this );
+    connect( update_left_time, SIGNAL( timeout() ), this, SLOT( update() ) );
+    update_left_time->start( 1000 );
+
+    ui->left_time->display( 6 - level );
+    left_time = 6 - level;
+    s         = _s;
+
+    qDebug() << "[left time]" << left_time;
 }
 
-void game::set_text(const QString _s) {
-    ui->question->setText(_s);
+void game::update() {
+    if ( left_time == 0 )
+        return;
+    else {
+        left_time--;
+        ui->left_time->display( left_time );
+        qDebug() << "[left time]" << left_time;
+    }
 }
 
-void game::cancel_context() {
-    ui->question->setText("");
-}
+void game::on_cancelBtn_clicked() { close(); }
 
-void game::start_game(const QString _s, int level) {
-    this->set_text(_s);
-    QTimer::singleShot(1000 * (6 - level), this, SLOT(cancel_context()));
-    s = _s;
-}
-
-void game::on_cancelBtn_clicked()
-{
-    close();
-}
-
-void game::on_OKBtn_clicked()
-{
-    if(ui->answer->text() == s)
+void game::on_OKBtn_clicked() {
+    if ( ui->answer->text() == s )
         accept();
     else
         close();
--- game.h
+++ game.h
@@ -7,26 +7,28 @@ namespace Ui {
 class game;
 }
 
-class game : public QDialog
-{
+class game : public QDialog {
     Q_OBJECT
 
-public:
-    explicit game(QWidget *parent = 0);
+  public:
+    explicit game( QWidget *parent = 0 );
     // void get_content(QString _s);
     ~game();
-    void start_game(const QString _s, int level);
+    void start_game( const QString _s, int level );
 
-private slots:
+  private slots:
     void on_cancelBtn_clicked();
 
     void on_OKBtn_clicked();
 
-    void set_text(const QString _s);
+    void set_text( const QString _s );
     void cancel_context();
-private:
+    void update();
+
+  private:
     Ui::game *ui;
-    QString s;
+    QString   s;
+    int       left_time;
 };
 
 #endif // GAME_H
